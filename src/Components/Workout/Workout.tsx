261769c37068bc8568980204e29925c049eff065
Aigars <aigars.uplejs@gmail.com>
2022-01-23 17:07:32 +0200

    Working timer prototype

--- src/Components/Workout/Workout.tsx
+++ src/Components/Workout/Workout.tsx
@@ -3,34 +3,48 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Workout: React.FC = () => {
-    const [time, setTime] = useState<number>(0);
-    const rounds = 3;
-    const roundTime = 5;
-    const restTime = 2;
-
-    const workout = {
-        rounds: 3,
-        roundTime: 5,
-        restTime: 2,
-        excercises: mockData
-    };
+    const totalRounds = 2;
+    const [currentRound, setCurrentRound] = useState(1);
+    const [isBreak, setIsBreak] = useState(true);
+    const roundLength = 10;
+    const breakLength = 5;
+    const [seconds, setSeconds] = useState(breakLength);
 
     useEffect(() => {
-        const timer = setTimeout(() => {
-            setTime((prevTime) => prevTime + 1);
-        }, 1000);
+        const intervalCounter = setInterval(() => {
+            // Check if timer needs to be stopped
+            if (currentRound <= totalRounds) {
+                if (seconds > 0) {
+                    setSeconds(seconds - 1);
+                }
+                if (seconds === 0) {
+                    if (isBreak) {
+                        setSeconds(roundLength);
+                    } else {
+                        // Increment round on round start
+                        setCurrentRound(currentRound + 1);
+                        setSeconds(breakLength);
+                    }
 
+                    // Switch from break to round and the other way around
+                    setIsBreak(!isBreak);
+                }
+            }
+
+            clearInterval(intervalCounter);
+        }, 1000);
         return () => {
-            clearTimeout(timer);
+            clearInterval(intervalCounter);
         };
-    }, []);
+    }, [currentRound, isBreak, seconds, totalRounds]);
 
     return (
         <div>
             <Link to="/">Home</Link>
-            <div>Round: {1}</div>
-            <div>Rest: {1} seconds</div>
-            <div>Round time left: {1} seconds</div>
+            <div>Round {currentRound}</div>
+            {isBreak ? <div>REST</div> : <div>WORK</div>}
+            <div>Time left: {seconds} seconds</div>
+            {currentRound > totalRounds && 'Workout over'}
         </div>
     );
 };
