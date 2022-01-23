35b98d4f351e3c02361adea67ea7c02bde46f173
Aigars <aigars.uplejs@gmail.com>
2022-01-23 20:50:54 +0200

    WIP Form processing

--- readme.md
+++ readme.md
@@ -1,8 +1,12 @@
 ## Base setup
 
 - Markdown formatting: https://www.markdownguide.org/basic-syntax/
-- React with ts andwebpack: https://muhammad-ata.medium.com/how-to-setup-react-typescript-project-from-scratch-with-webpack-fa45a90c7a4d
+- React with ts and webpack: https://muhammad-ata.medium.com/how-to-setup-react-typescript-project-from-scratch-with-webpack-fa45a90c7a4d
 
+
+## Form handling
+
+- https://www.digitalocean.com/community/tutorials/how-to-build-forms-in-react
 ## Linter and prettier
 
 - yarn add eslint-plugin-react-hooks eslint-plugin-react -D
--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,36 +1,108 @@
 import { mockData } from '@App/MockApi/mockData';
-import React from 'react';
+import React, { useReducer } from 'react';
 import { useNavigate } from 'react-router-dom';
+
+
+// TODO types
+const formReducer = (state: any, event: { [key: string]: any }) => {
+    return {
+        ...state,
+        [event.name]: event.value
+    };
+};
+
+const defaultState = {
+    roundLength: 3,
+    breakLength: 2,
+    totalRounds: 5
+};
 
 const Home: React.FC = () => {
     const navigate = useNavigate();
+    const [formData, setFormData] = useReducer(formReducer, defaultState);
+    const types = [
+        {
+            label: 'No Excercise',
+            code: 'noExcercise'
+        },
+        {
+            label: 'Abs',
+            code: 'abs'
+        },
+        {
+            label: 'Legs',
+            code: 'legs'
+        },
+        {
+            label: 'Chest',
+            code: 'chest'
+        },
+        {
+            label: 'Triceps',
+            code: 'triceps'
+        },
+        {
+            label: 'Warm Up',
+            code: 'warmUp'
+        },
+        {
+            label: 'Shoulders',
+            code: 'shoulders'
+        }
+    ];
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement> & React.ChangeEvent<HTMLSelectElement>) => {
+        const isCheckbox = event.target.type === 'checkbox';
+        setFormData({
+            name: event.target.name,
+            value: isCheckbox ? event.target.checked : event.target.value
+        });
+    };
+
+    console.log(formData);
 
     return (
         <form
-            onSubmit={(event) => {
+            onSubmit={(event: React.SyntheticEvent) => {
                 event.preventDefault();
-                navigate('/workout');
+                // navigate('/workout');
             }}
         >
-            <label htmlFor="roundTime">Round duration</label>
-            <input type="number" defaultValue={1} name="roundTime" />
+            <label htmlFor="roundLength">Round duration</label>
+            <input type="number" defaultValue={1} name="roundLength" onChange={handleChange} />
 
-            <label>Rest duration</label>
-            <input type="number" defaultValue={1} />
+            <label>Break duration</label>
+            <input type="number" defaultValue={1} name="breakLength" onChange={handleChange} />
 
             <label>Round count</label>
-            <input type="number" defaultValue={1} />
+            <input type="number" defaultValue={1} name="totalRounds" onChange={handleChange} />
+
+            <label>
+           <p>Apples</p>
+           <select name="apple" onChange={handleChange}>
+               <option value="">--Please choose an option--</option>
+               <option value="fuji">Fuji</option>
+               <option value="jonathan">Jonathan</option>
+               <option value="honey-crisp">Honey Crisp</option>
+           </select>
+         </label>
 
             <fieldset>
                 <legend>Select excercise types</legend>
-                <div>
-                    <input type="checkbox" name="abs" value="abs" />
-                    <label htmlFor="abs">Abs</label>
-                </div>
-                <div>
-                    <input type="checkbox" name="legs" value="legs" />
-                    <label htmlFor="legs">Legs</label>
-                </div>
+                {types.map((type) => {
+                    return (
+                        <div key={type.code}>
+                            <input
+                                type="checkbox"
+                                id={type.code}
+                                name={type.code}
+                                value={type.code}
+                                onChange={handleChange}
+                            />
+                            <label htmlFor={type.code}>{type.label}</label>
+                        </div>
+                    );
+                })}
             </fieldset>
 
             <button>Button</button>
