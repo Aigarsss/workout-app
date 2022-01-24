e03846e95649e07aa255e327016445f18fc24f3f
Aigars <aigars.uplejs@gmail.com>
2022-01-24 22:30:18 +0200

    Update form with dummy data

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,55 +1,115 @@
-import { mockData } from '@App/MockApi/mockData';
 import React, { useReducer } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-
-// TODO types
-const formReducer = (state: any, event: { [key: string]: any }) => {
+const formReducer = (state: any, event: { name: string; value: boolean | number | string }) => {
     return {
         ...state,
         [event.name]: event.value
     };
 };
 
+const DEFAULT_ROUND_LENGTH = 60;
+const DEFAULT_BREAK_LENGTH = 15;
+const DEFAULT_TOTAL_ROUNDS = 5;
+
 const defaultState = {
-    roundLength: 3,
-    breakLength: 2,
-    totalRounds: 5
+    roundLength: DEFAULT_ROUND_LENGTH,
+    breakLength: DEFAULT_BREAK_LENGTH,
+    totalRounds: DEFAULT_TOTAL_ROUNDS,
+    noExercise: false,
+    abs: false,
+    legs: false,
+    chest: false,
+    triceps: false,
+    warmUp: false,
+    shoulders: false
 };
+
+const types = [
+    {
+        label: 'No Exercise',
+        code: 'noExercise'
+    },
+    {
+        label: 'Abs',
+        code: 'abs'
+    },
+    {
+        label: 'Legs',
+        code: 'legs'
+    },
+    {
+        label: 'Chest',
+        code: 'chest'
+    },
+    {
+        label: 'Triceps',
+        code: 'triceps'
+    },
+    {
+        label: 'Warm Up',
+        code: 'warmUp'
+    },
+    {
+        label: 'Shoulders',
+        code: 'shoulders'
+    }
+];
+
+const durations = [
+    {
+        label: '10s',
+        value: '10'
+    },
+    {
+        label: '15s',
+        value: '15'
+    },
+    {
+        label: '30s',
+        value: '30'
+    },
+    {
+        label: '45s',
+        value: '45'
+    },
+    {
+        label: '1m',
+        value: '60'
+    },
+    {
+        label: '1.5m',
+        value: '90'
+    },
+    {
+        label: '2m',
+        value: '120'
+    },
+    {
+        label: '3m',
+        value: '180'
+    },
+    {
+        label: '4m',
+        value: '240'
+    },
+    {
+        label: '5m',
+        value: '300'
+    },
+    {
+        label: '10m',
+        value: '600'
+    },
+    {
+        label: '20m',
+        value: '1200'
+    }
+];
 
 const Home: React.FC = () => {
     const navigate = useNavigate();
     const [formData, setFormData] = useReducer(formReducer, defaultState);
-    const types = [
-        {
-            label: 'No Excercise',
-            code: 'noExcercise'
-        },
-        {
-            label: 'Abs',
-            code: 'abs'
-        },
-        {
-            label: 'Legs',
-            code: 'legs'
-        },
-        {
-            label: 'Chest',
-            code: 'chest'
-        },
-        {
-            label: 'Triceps',
-            code: 'triceps'
-        },
-        {
-            label: 'Warm Up',
-            code: 'warmUp'
-        },
-        {
-            label: 'Shoulders',
-            code: 'shoulders'
-        }
-    ];
 
     const handleChange = (event: React.ChangeEvent<HTMLInputElement> & React.ChangeEvent<HTMLSelectElement>) => {
         const isCheckbox = event.target.type === 'checkbox';
@@ -68,24 +128,41 @@ const Home: React.FC = () => {
                 // navigate('/workout');
             }}
         >
-            <label htmlFor="roundLength">Round duration</label>
-            <input type="number" defaultValue={1} name="roundLength" onChange={handleChange} />
-
-            <label>Break duration</label>
-            <input type="number" defaultValue={1} name="breakLength" onChange={handleChange} />
-
-            <label>Round count</label>
-            <input type="number" defaultValue={1} name="totalRounds" onChange={handleChange} />
+            <label>
+                <p>Round duration</p>
+                <select name="roundLength" onChange={handleChange} defaultValue={DEFAULT_ROUND_LENGTH}>
+                    {durations.map((duration) => (
+                        <option value={duration.value} key={duration.value}>
+                            {duration.label}
+                        </option>
+                    ))}
+                </select>
+            </label>
 
             <label>
-           <p>Apples</p>
-           <select name="apple" onChange={handleChange}>
-               <option value="">--Please choose an option--</option>
-               <option value="fuji">Fuji</option>
-               <option value="jonathan">Jonathan</option>
-               <option value="honey-crisp">Honey Crisp</option>
-           </select>
-         </label>
+                <p>Break duration</p>
+                <select name="breakLength" onChange={handleChange} defaultValue={DEFAULT_BREAK_LENGTH}>
+                    {durations.map((duration) => (
+                        <option value={duration.value} key={duration.value}>
+                            {duration.label}
+                        </option>
+                    ))}
+                </select>
+            </label>
+
+            <label>
+                <p>Total rounds</p>
+                <select name="totalRounds" onChange={handleChange} defaultValue={DEFAULT_TOTAL_ROUNDS}>
+                    {Array.from(Array(20).keys()).map((option) => {
+                        const val = option + 1;
+                        return (
+                            <option value={val} key={val}>
+                                {val}
+                            </option>
+                        );
+                    })}
+                </select>
+            </label>
 
             <fieldset>
                 <legend>Select excercise types</legend>
